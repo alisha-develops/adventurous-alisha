464f9bb93be9bc8c90de64fbaf2e4cdd7ba89653
alisha-develops <alishalish107@gmail.com>
2026-06-20 01:56:53 +0500

    add backdrop for open book interaction and update related styles and scripts

--- script.js
+++ script.js
@@ -14,13 +14,16 @@ tl.to("#planesvg", {
 
 const openbook = document.getElementById("openbook");
 const closedbook = document.getElementById("closebook");
+const backdrop = document.getElementById("backdrop");
 
 document.getElementById("closedbook").addEventListener("click", () => {
     openbook.classList.toggle("active");
     closedbook.classList.toggle("active");
+    backdrop.classList.toggle("active");
 });
 
 closedbook.addEventListener("click", () => {
     openbook.classList.remove("active");
     closedbook.classList.remove("active");
+    backdrop.classList.remove("active");
 });
