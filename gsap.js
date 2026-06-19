52b2eb46db5103bc054a42cef0309dbdd73df7f0
alisha-develops <alishalish107@gmail.com>
2026-06-19 17:31:20 +0500

    improve animation logic: move GSAP animation from gsap.js to script.js and remove unused gsap.js file

--- gsap.js
+++
@@ -1,6 +0,0 @@
-gsap.to("#planesvg", {
-    left: "60%",
-    duration: 5,
-    ease: "power1.inOut",
-    delay: 0.5
-})
--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-// document.getElementById("planesvg").src = "svgcode/planedoor.svg";
+let tl = gsap.timeline();
+
+tl.to("#planesvg", {
+    left: "60%",
+    duration: 5,
+    ease: "power1.inOut",
+    delay: 0.5
+})
+.call(() => {
+    document.getElementById("planesvg").src = "svgcode/planedoor.svg"
+})
+.to("#planesvg", {
+    x: -5,
+    duration: 1.0,
+    yoyo: true,
+    repeat: 3
+})
