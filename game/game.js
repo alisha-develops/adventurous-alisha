33331a04e70de6046fcc5975366d5474ec65069e
alisha-develops <alishalish107@gmail.com>
2026-07-05 01:42:11 +0500

    game mechanics: add player movement, target behavior, and score tracking

--- game/game.js
+++ game/game.js
@@ -7,16 +7,63 @@ const speedDown = 300
 
 class GameScene extends Phaser.Scene{
     constructor(){
-        super("scene-game")
+        super("scene-game");
+        this.player;
+        this.cursor;
+        this.playerSpeed = speedDown + 50;
+        this.target;
+        this.points = 0;
+        this.textScore
     }
 
     preload(){
         this.load.image("bg", "assets/bg.png")
+        this.load.image("basket", "assets/basket.png")
+        this.load.image("apple", "assets/apple.png")
     }
     create(){
-        this.add.image(0,0,"bg")
+        this.add.image(0,0,"bg").setOrigin(0,0)
+        this.player = this.physics.add.image(0,sizes.height-100, "basket").setOrigin(0,0)
+        this.player.setImmovable(true);
+        this.player.body.allowGravity = false;
+        this.player.setCollideWorldBounds(true);
+        this.player.setSize(80, 15).setOffset(10, 70)
+
+        this.target = this.physics.add.image(0,sizes.height-100, "apple").setOrigin(0,0)
+        this.target.setMaxVelocity(0, speedDown);
+
+        this.physics.add.overlap(this.target, this.player, this.targetHit, null, this)
+
+        this.cursor = this.input.keyboard.createCursorKeys();
+
+        this.textScore = this.add.text(sizes.width - 120, 10, "score:0", {
+            font: "25px Stone Breaker",
+            fill: "#006400",
+        });
     }
-    update(){}
+    update(){
+        if (this.target.y >= sizes.height){
+            this.target.setY(0);
+            this.target.setX(this.getRandomX())
+        }
+        const {left,right} = this.cursor
+
+        if (left.isDown) {
+            this.player.setVelocityX(-this.playerSpeed);
+        } else if (right.isDown){
+            this.player.setVelocityX(this.playerSpeed);
+        } else{
+            this.player.setVelocityX(0);
+        }
+    }
+    getRandomX() {
+        return Math.floor(Math.random() * 480);
+    }
+    targetHit() {
+        this.target.setY(0);
+        this.target.setX(this.getRandomX());
+        this.points++;
+    }
 }
 
 const config = {
